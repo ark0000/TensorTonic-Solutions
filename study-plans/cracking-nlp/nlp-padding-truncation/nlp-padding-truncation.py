b94852d7e9bd6175f310697592aa30e6e065ef68
ark0000 <59128219+ark0000@users.noreply.github.com>
2026-04-18 17:52:24 +0530

    Add solution for nlp-padding-truncation

---
+++ study-plans/cracking-nlp/nlp-padding-truncation/nlp-padding-truncation.py
@@ -0,0 +1,19 @@
+def pad_and_truncate(sequences, max_length, pad_value=0):
+    t=[]
+    for i in sequences:
+        if(len(i)<max_length):
+            times=max_length-len(i)
+            
+            for _ in range(0,times):
+                
+                i.extend([pad_value])
+            t.append(i)
+        else:
+          t.append(i[:max_length])
+                
+    return t            
+    
+    """
+    Returns: list[list[int]]
+    """
+    pass
